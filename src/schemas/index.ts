70d7b941fbdfc9a7806acb92c61be7ff8cb2952b
Jose Luis <jcarrizo.market@gmail.com>
2026-02-26 15:06:45 +0100

    Create index.ts for schema exports

---
+++ src/schemas/index.ts
@@ -0,0 +1,6 @@
+// Schema exports
+export * from './pedido.schema';
+export * from './camarero.schema';
+export * from './coordinador.schema';
+export * from './cliente.schema';
+export * from './mensajes.schema';
